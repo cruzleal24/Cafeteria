6a7b06aed7c5e995f33be165cd7572172c4e2535
cruzleal24 <falexiscl@gmail.com>
2025-05-23 00:51:55 -0600

    modificacion

--- js/subtotal.js
+++ js/subtotal.js
@@ -2,6 +2,6 @@ const cantidad= document.querySelector("input");
 const subtotal= document.querySelector("subtotal");
 
 cantidad.addEventListener("change", (e)=>{
-    subtotal.textContent=subtotal+ e.currentTarget.value;
+    subtotal.textContent= e.currentTarget.value;
     console.log( sizeof(subtotal));
 });
